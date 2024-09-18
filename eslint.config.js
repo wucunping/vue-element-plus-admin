9a72e7acd3d657abe850a3160e3ad83ed7b3cce1
6988116@qq.com <6988116@qq.com>
2024-09-18 14:14:00 +0800

    搭建项目脚手架

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+  {
+    ignores: ["src/test.ts"],  // 全局忽略 src/test.ts 文件
+  },
+];
